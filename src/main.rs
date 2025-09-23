d9652f80489f1e5815fed9d55133e06472a12594
Jackson Walters <jacksonwalters@gmail.com>
2025-09-23 16:32:07 -0400

    remove "message:" from print statement
    when printing to the console, just print exactly what the message is

--- src/main.rs
+++ src/main.rs
@@ -188,7 +188,7 @@ fn main() {
             };
 
             let message = decrypt_string(&sk_string, &ct_string, &params);
-            println!("message: {:?}", message);
+            println!("{:?}", message);
         }
     }
 }
